f2ae66dcfdf500198a44d01562c0daac0d39581a
christian otkjær <christian.otkjaer@gmail.com>
2016-10-25 14:40:52 +0200

    Initial Commit

---
+++ Plus/Plus.h
@@ -0,0 +1,19 @@
+//
+//  Plus.h
+//  Plus
+//
+//  Created by Christian Otkjær on 25/10/16.
+//  Copyright © 2016 Silverback IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Plus.
+FOUNDATION_EXPORT double PlusVersionNumber;
+
+//! Project version string for Plus.
+FOUNDATION_EXPORT const unsigned char PlusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Plus/PublicHeader.h>
+
+
