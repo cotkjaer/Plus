26f64c4046130b6ce248a8a3af9998e71d43788d
christian otkjær <christian.otkjaer@gmail.com>
2016-10-25 14:50:43 +0200

    Alpha

---
+++ Plus/FileManager.swift
@@ -0,0 +1,51 @@
+//
+//  FileManager.swift
+//  Plus
+//
+//  Created by Christian Otkjær on 25/10/16.
+//  Copyright © 2016 Christian Otkjær. All rights reserved.
+//
+
+import Foundation
+
+public extension FileManager
+{
+    @discardableResult
+    func copy(resourceNamed name: String, ofType: String, inBundle bundle: Bundle = Bundle.main, toFolder folderPath: String) throws -> String?
+    {
+        let pathToFile = folderPath.appending("/\(name).\(ofType)")
+        
+        guard !FileManager.default.fileExists(atPath: pathToFile) else { return nil }
+        
+        guard let pathToBundledResource = bundle.path(forResource: name, ofType: ofType) else { return nil }
+        
+        try FileManager.default.copyItem(atPath: pathToBundledResource, toPath: pathToFile)
+        
+        return pathToFile
+    }
+    
+    func getDataFromUrl(url: URL, completion: @escaping (_ data: Data?, _ response: URLResponse?, _ error: Error? ) -> ())
+    {
+        URLSession.shared.dataTask(with: url) {
+            (data, response, error) in
+            completion(data, response, error)
+            }.resume()
+    }
+    
+    func documentsFolderURL() -> URL?
+    {
+        guard let path = documentsFolderPath() else { return nil }
+        
+        return URL(fileURLWithPath: path, isDirectory: true)
+    }
+    
+    func documentURL(forFile fileName: String, fileExtension: String) -> URL?
+    {
+        return documentsFolderURL()?.appendingPathComponent(fileName).appendingPathExtension(fileExtension)
+    }
+    
+    func documentsFolderPath() -> String?
+    {
+        return NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).last
+    }
+}
---
+++ PlusTests/AsyncTests.swift
@@ -0,0 +1,64 @@
+//
+//  AsyncTests.swift
+//  Plus
+//
+//  Created by Christian Otkjær on 18/09/15.
+//  Copyright © 2015 Christian Otkjær. All rights reserved.
+//
+
+import XCTest
+
+@testable import Plus
+
+class AsyncTests: XCTestCase
+{
+    func test_delay_seconds()
+    {
+        let expectation = self.expectation(description: "delayed closure did run")
+        
+        let now = NSDate()
+        var when = NSDate()
+        
+        delay(3) {
+            
+            when = NSDate()
+            expectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 4)
+        {
+            XCTAssertNil($0)
+            
+            XCTAssertGreaterThan(when.timeIntervalSinceReferenceDate, now.timeIntervalSinceReferenceDate + 2)
+        }
+    }
+    
+    func testDelay()
+    {
+        let expectation = self.expectation(description: "delayed closure did run")
+
+        var counter = 0
+        
+        delay(0.2) {
+            
+            counter += 1
+            
+            XCTAssertGreaterThan(counter, 1)
+            
+            expectation.fulfill()
+        }
+        
+        XCTAssertEqual(counter, 0)
+        
+        counter += 1
+
+        XCTAssertEqual(counter, 1)
+
+        waitForExpectations(timeout: 1) {
+
+            XCTAssertNil($0)
+            
+            XCTAssertEqual(counter, 2)
+        }
+    }
+}
